7dba4920617ac904ee05ec6263649c674545b634
sanshishao <1847319931@qq.com>
2017-04-10 14:36:14 +0800

    修改pom

---
+++ src/main/java/com/liusl/dao/IUserDao.java
@@ -0,0 +1,7 @@
+package com.liusl.dao;
+
+/**
+ * Created by lsl on 2017/4/10.
+ */
+public class IUserDao {
+}
---
+++ src/main/java/com/liusl/model/User.java
@@ -0,0 +1,7 @@
+package com.liusl.model;
+
+/**
+ * Created by lsl on 2017/4/10.
+ */
+public class User {
+}
